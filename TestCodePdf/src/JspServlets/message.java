4762788029de3c9eb67c6277bab135c7c3a789b7
Thib <tibhault145@gmail.com>
2017-11-09 14:22:58 +0100

    maj

---
+++ TestCodePdf/src/JspServlets/message.java
@@ -0,0 +1,25 @@
+package JspServlets;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author tibha
+ */
+public class message
+{
+private String texteMessage;
+public message()
+{
+    texteMessage = null;
+}
+public message(String m)
+{
+texteMessage = m;
+}
+public String getTexteMessage() { return texteMessage; }
+public void setTexteMessage (String m) { texteMessage = m; }
+}
